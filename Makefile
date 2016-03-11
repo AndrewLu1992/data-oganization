2938f1ba7ed66fe5adac37ab53a5c82ba07d4bdc
Shuo Yan <sure661@gmail.com>
2016-03-10 23:37:16 -0600

    add basic function for record function

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
-OBJS=dberror.o storage_mgr.o test_assign2_1.o buffer_mgr.o buffer_mgr_stat.o buffer_mgr_algorithm.o
+OBJS=dberror.o storage_mgr.o test_assign3_1.o buffer_mgr.o buffer_mgr_stat.o buffer_mgr_algorithm.o expr.o record_mgr.o rm_serializer.o
 
 all: $(OBJS)
-	cc -o test_assign2_1 $(OBJS)
+	cc -o test_assign3_1 $(OBJS)
 
 dberror.o: dberror.c dberror.h
 	cc -c dberror.c
@@ -9,8 +9,11 @@ dberror.o: dberror.c dberror.h
 storage_mgr.o: storage_mgr.c storage_mgr.h dberror.h
 	cc -c storage_mgr.c
 
-test_lfu.o: test_assign2_1.c test_helper.h storage_mgr.h dberror.h
-	cc -c test_assign2_1.c
+test_assign3_1.o: test_assign3_1.c test_helper.h storage_mgr.h dberror.h
+	cc -c test_assign3_1.c rm_serializer.c
+
+rm_serializer.o: rm_serializer.c tables.h
+	cc -c rm_serializer.c
 
 buffer_mgr.o: buffer_mgr.c buffer_mgr.h
 	cc -c buffer_mgr.c
@@ -18,9 +21,15 @@ buffer_mgr.o: buffer_mgr.c buffer_mgr.h
 buffer_mgr_algorithm.o: buffer_mgr_algorithm.c buffer_mgr_algorithm.h
 	cc -c buffer_mgr_algorithm.c
 
-buffer_mgr_stat.o:buffer_mgr_stat.c  buffer_mgr_stat.h buffer_mgr_algorithm.h
+buffer_mgr_stat.o: buffer_mgr_stat.c  buffer_mgr_stat.h buffer_mgr_algorithm.h
 	cc -c buffer_mgr_stat.c 
+
+expr.o: expr.c expr.h
+	cc -c expr.c
+
+record_mgr.o: record_mgr.c record_mgr.h
+	cc -c record_mgr.c
 
 .PHONY: clean
 clean:
-	-rm *.o test_assign2_1  
+	-rm *.o test_assign3_1  
--- record_mgr.c
+++ record_mgr.c
@@ -1,132 +1,190 @@
 #include "record_mgr.h"
+#include "buffer_mgr_stat.h"
+#include "dberror.h"
+#include "buffer_mgr.h"
+
+
+BM_BufferPool *bm;
 
 // table and manager
 RC initRecordManager (void *mgmtData){
-	int ret;
+	int ret = 0;
+
+    bm = MAKE_POOL();
+    
+    ret = createPageFile("database.bin");
+    if(ret != RC_OK) {
+        printf("Create Page Fail\n");
+        return ret;
+    }
+    
+    ret = initBufferPool(bm, "database.bin", 5, RS_LFU, NULL);
+    if (ret != RC_OK) {
+        printf("Init Buffer POOL Fail\n");
+        return RC_BM_BP_INIT_BUFFER_POOL_FAILED;
+    }
 
 	return ret;
 }
 
 RC shutdownRecordManager (){
-	int ret;
+	int ret = 0;
+
+    ret = shutdownBufferPool(bm);
+    if(ret != 0) {
+        printf("shutdown buffer pool fail\n");
+        return ret;
+    }
+        
+    bm = NULL;
 
 	return ret;
 }
 
 RC createTable (char *name, Schema *schema) {
-	int ret;
+	int ret = 0;
+
+    printf("Enter %s\n", __func__);
+    
+    if(schema == NULL) {
+        printf("schema is NULL\n");
+        return RC_REC_TABLE_CREATE_FAILED;
+    }
+    
+    ret = createPageFile(name);
+    
+    if (ret != RC_OK) {
+        printf("Create Table Page File %s Fail\n", name);
+        return RC_REC_TABLE_CREATE_FAILED;
+    }
+
+    
 
 	return ret;
 }
 
 RC openTable (RM_TableData *rel, char *name) {
-	int ret;
+	int ret = 0;
 
 	return ret;
 }
 
 RC closeTable (RM_TableData *rel){
-	int ret;
+	int ret = 0;
 
 	return ret;
 }
 
 RC deleteTable (char *name){
-	int ret;
+	int ret = 0;
 
 	return ret;
 }
 
 int getNumTuples (RM_TableData *rel) {
-	int ret;
+	int ret = 0;
 
 	return ret;
 }
 
 // handling records in a table
 RC insertRecord (RM_TableData *rel, Record *record) {
-	int ret;
+	int ret = 0;
 
 	return ret;
 }
 
 RC deleteRecord (RM_TableData *rel, RID id) {
-	int ret;
+	int ret = 0;
 
 	return ret;
 }
 
 RC updateRecord (RM_TableData *rel, Record *record) {
-	int ret;
+	int ret = 0;
 
 	return ret;
 }
 
 RC getRecord (RM_TableData *rel, RID id, Record *record) {
-	int ret;
+	int ret = 0;
 
 	return ret;
 }
 
 // scans
 RC startScan (RM_TableData *rel, RM_ScanHandle *scan, Expr *cond) {
-	int ret;
+	int ret = 0;
 
 	return ret;
 }
 
 RC next (RM_ScanHandle *scan, Record *record) {
-	int ret;
+	int ret = 0;
 
 	return ret;
 }
 
 RC closeScan (RM_ScanHandle *scan) {
-	int ret;
+	int ret = 0;
 
 	return ret;
 }
 
 // dealing with schemas
 int getRecordSize (Schema *schema) {
-	int ret;
+	int ret = 0;
 
 	return ret;
 }
 
 Schema *createSchema (int numAttr, char **attrNames, DataType *dataTypes, int *typeLength, int keySize, int *keys) {
-	int ret;
+	int ret = 0;
+    Schema *schm;
 
-	return ret;
+    schm = malloc(sizeof(Schema));
+    if (schm == NULL) {
+        printf("malloc schema size fail\n");
+        return schm;
+    }
+    
+    schm->numAttr = numAttr; 
+    schm->attrNames = attrNames;
+    schm->dataTypes = dataTypes;
+    schm->typeLength = typeLength;
+    schm->keyAttrs = keys; 
+    schm->keySize = keySize;
+
+	return schm;
 }
 
 RC freeSchema (Schema *schema){
-	int ret;
+	int ret = 0;
 
 	return ret;
 }
 
 // dealing with records and attribute values
 RC createRecord (Record **record, Schema *schema) {
-	int ret;
+	int ret = 0;
 
 	return ret;
 }
 
 RC freeRecord (Record *record) {
-	int ret;
+	int ret = 0;
 
 	return ret;
 }
 
 RC getAttr (Record *record, Schema *schema, int attrNum, Value **value) {
-	int ret;
+	int ret = 0;
 
 	return ret;
 }
 
 RC setAttr (Record *record, Schema *schema, int attrNum, Value *value) {
-	int ret;
+	int ret = 0;
 
 	return ret;
 }
